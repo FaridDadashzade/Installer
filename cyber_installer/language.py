feedbb9b4850a9f045440c032e039c8ba51d3b33
FaridDadashzade <73734920+FaridDadashzade@users.noreply.github.com>
2021-05-21 14:59:51 +0400

    fix

--- cyber_installer/language.py
+++ cyber_installer/language.py
@@ -5,7 +5,7 @@ from . import logo, console, bilgi
 def importlang ():
     console.clear()
     logo()
-    bilgi("\n\[1] Türkçe\n\[2] Azərbaycanca\n\[3] English\n\[4] O'zbek")
+    bilgi("\n\[1] Azərbaycanca\n\[2] Türkçe\n\[3] English\n\[4] O'zbek")
     Dil = Prompt.ask("[bold yellow]Xahiş edirəm bir dil seçin [/]", choices=["1", "2", "3", "4"], default="1")
 
     if Dil == "1":
