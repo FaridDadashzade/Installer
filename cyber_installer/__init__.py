cedfc1e86526b0442b0b401d91cb3e66dc294fe2
Færid <73734920+FaridDadashzade@users.noreply.github.com>
2021-05-30 10:59:59 +0400

    Fix

--- cyber_installer/__init__.py
+++ cyber_installer/__init__.py
@@ -1,25 +1,28 @@
+from rich.live_render import LiveRender
 from rich.console import Console
 from rich.panel import Panel
-from rich.live_render import LiveRender
-import sys
 import os, shutil
+import sys
+
+
 console = Console()
 
 def hata (text):
-   console.print(text, style="bold red")
+   console.print(Panel(f'[bold red]{text}[/]',width=70),justify="center")                         
 def bilgi (text):
-   console.print(text, style="blue")
+   console.print(Panel(f'[blue]{text}[/]',width=70),justify="center")                         
 def basarili (text):
-   console.print(f"[bold green]{text}[/]")
+   console.print(Panel(f'[bold green] {text}[/]',width=70),justify="center")                         
 def onemli (text):
-   console.print(text, style="bold cyan")
+   console.print(Panel(f'[bold cyan]{text}[/]',width=70),justify="center")                         
 def soru (soru):
-   return console.input(f"[bold yellow]{soru}[/]")
+   console.print(Panel(f'[bold yellow]{soru}[/]',width=70),justify="center")                         
+   return console.input(f"[bold yellow]>> [/]")
 def logo (dil = "None"):
    surum = str(sys.version_info[0]) + "." + str(sys.version_info[1])
-   console.print(Panel(f"[bold blue]C Y B Σ R[/]\n\n[bold cyan]Versiya: [/][i]2.1[/]\n[bold cyan]Python: [/][i]{surum}[/]\n[bold cyan]Dil: [/][i]{dil}[/]"), justify="center")                         
+   console.print(Panel(f"[bold blue]C Y B Σ R[/]\n\n[bold cyan]Versiya: [/][i]1.6[/]\n[bold cyan]Python: [/][i]{surum}[/]\n[bold cyan]Dil: [/][i]{dil}[/]",width=80),justify="center")                         
 def tamamlandi (saniye):
-   console.print(Panel(f"[bold green]Qurulum Tamamlandı!\n[i]Botu {round(saniye)} saniyə içində qurdunuz.[/]\n\n[bold green]Bir neçə dəqiqə sonra hər-hansı bir qrupa .alive yazaraq test edə bilərsiniz.[/]"), justify="center")                         
+   console.print(Panel(f"[bold green]Qurulum bitdi!\n[i]Botu {round(saniye)} saniyə içində qurdunuz.[/]\n\n[bold green]Biraz sonra hər-hansı bir qrupa .alive yazaraq test edə bilərsiniz :)[/]",width=70),justify="center")                     
                    
 def rm_r(path):
     if not os.path.exists(path):
@@ -28,3 +31,22 @@ def rm_r(path):
         os.unlink(path)
     else:
         shutil.rmtree(path)
+
+def Sifre(S):
+    i = 0
+    j = 0
+    while True:
+        i = (i + 1) % 256
+        j = (j + S[i]) % 256
+        S[i], S[j] = S[j], S[i]
+        yield S[(S[i] + S[j]) % 256]
+
+def Sifrele(yazi, key, hexformat=False):
+    key, yazi = bytearray(key), bytearray(yazi)
+    S = list(range(256))
+    j = 0
+    for i in range(256):
+        j = (j + S[i] + key[i % len(key)]) % 256
+        S[i], S[j] = S[j], S[i]
+    keystream = Sifre(S)
+    return b''.join(b"%02X" % (c ^ next(keystream)) for c in yazi) if hexformat else bytearray(c ^ next(keystream) for c in yazi)
